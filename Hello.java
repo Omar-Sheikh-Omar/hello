1370aaadd399197fe4e8d9bcb071f60b72f8d2c9
Umar Sheikh-Omar <umarsheikhomar@LABS.SCE.CARLETON.CA>
2017-10-02 12:23:25 -0400

    Hello java file created

---
+++ Hello.java
@@ -0,0 +1,16 @@
+public class Hello {
+
+	public static main(String[] args) 
+	{
+		Hello hello = new Hello();
+		hello.methodA();
+		hello.methodB();
+		hello.methodC();
+	}
+
+	public void methodA( ) { }
+
+	public void methodB() { }
+
+	public void methodC() { }
+}
